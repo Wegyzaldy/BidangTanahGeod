48d8d4b1b182e03091fe6e3bca1dba5624f2db21
jemappelewegykkk <56816266+Wegyzaldy@users.noreply.github.com>
2022-11-27 23:50:37 +0700

    Add files via upload

---
+++ TBT.js
@@ -0,0 +1,17 @@
+var TBT = {
+"type": "FeatureCollection",
+"name": "TBT",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Titik": "100", "X": 296223.358, "Y": 641446.617, "Z": "173.781", "KODE": "BDG3" }, "geometry": { "type": "Point", "coordinates": [ 110.372387415708459, -7.763900684798074 ] } },
+{ "type": "Feature", "properties": { "Titik": "101", "X": 296240.156, "Y": 641495.337, "Z": "174.42", "KODE": "BDG4" }, "geometry": { "type": "Point", "coordinates": [ 110.372538790246054, -7.763459851620397 ] } },
+{ "type": "Feature", "properties": { "Titik": "102", "X": 296182.292, "Y": 641471.83, "Z": "173.844", "KODE": "BDG4" }, "geometry": { "type": "Point", "coordinates": [ 110.372014659552136, -7.763673475337375 ] } },
+{ "type": "Feature", "properties": { "Titik": "103", "X": 296193.057, "Y": 641462.127, "Z": "173.783", "KODE": "BDG4" }, "geometry": { "type": "Point", "coordinates": [ 110.372112431113294, -7.763761008625882 ] } },
+{ "type": "Feature", "properties": { "Titik": "105", "X": 296207.706, "Y": 641451.845, "Z": "173.776", "KODE": "BDG4" }, "geometry": { "type": "Point", "coordinates": [ 110.372245424130398, -7.763853704911921 ] } },
+{ "type": "Feature", "properties": { "Titik": "106", "X": 296230.624, "Y": 641444.095, "Z": "173.726", "KODE": "BDG3" }, "geometry": { "type": "Point", "coordinates": [ 110.372453333079676, -7.763923353285163 ] } },
+{ "type": "Feature", "properties": { "Titik": "108", "X": 296256.843, "Y": 641467.847, "Z": "174.109", "KODE": "BDG3" }, "geometry": { "type": "Point", "coordinates": [ 110.372690579830845, -7.763708102634514 ] } },
+{ "type": "Feature", "properties": { "Titik": "109", "X": 296264.725, "Y": 641479.887, "Z": "174.354", "KODE": "BDG3" }, "geometry": { "type": "Point", "coordinates": [ 110.37276180989511, -7.763599091670403 ] } },
+{ "type": "Feature", "properties": { "Titik": "113", "X": 296297.25, "Y": 641463.525, "Z": "174.252", "KODE": "BDG2" }, "geometry": { "type": "Point", "coordinates": [ 110.373056972234309, -7.763746429701435 ] } },
+{ "type": "Feature", "properties": { "Titik": "114", "X": 296296.731, "Y": 641460.374, "Z": "174.055", "KODE": "BDG2" }, "geometry": { "type": "Point", "coordinates": [ 110.373052326017813, -7.763774930342329 ] } }
+]
+}
